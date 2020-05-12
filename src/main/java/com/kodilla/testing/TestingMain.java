7067b12b26800117bc7dc7e0abb905bdb0f6eed7
Karolina Bitner <karolabitner@gmail.com>
2020-05-12 20:02:35 +0200

    Calculator

--- src/main/java/com/kodilla/testing/TestingMain.java
+++ src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,19 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        // tu umieść kolejny test ->
+        System.out.println("Test - pierwszy test jednostkowy:");
     }
 }
--- src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -1,4 +1,12 @@
 package com.kodilla.testing.calculator;
 
 public class Calculator {
+        int a;
+        int b;
+        public int add(int a, int b){
+            return a + b;
+        }
+        public int substract(int a, int b){
+            return a - b;
+        }
 }
