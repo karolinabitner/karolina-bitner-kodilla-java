300fa484803d26429b71e67a182100cc97f7711f
Karolina Bitner <karolabitner@gmail.com>
2020-05-13 20:54:09 +0200

    collection

--- src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,4 +1,14 @@
 package com.kodilla.testing.collection;
 
 public class OddNumbersExterminator {
+    public int exterminate(ArrayList<Integer> numbers){
+        ArrayList<Integer>oddNumbersList = new ArrayList<>();
+
+        for(int number : numbers){
+            if(number%2==0){
+                oddNumbersList.add(number);
+            }
+        }
+        return oddNumbersList;
+    }
 }
--- src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,4 +1,56 @@
 package com.kodilla.testing.collection;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class CollectionTestSuite {
+    Object oddNumbersExterminator;
+    @Before
+    public void before(){
+        System.out.println("Test: begin");
+    }
+    @After
+    public void after(){
+        System.out.println("Test: end");
+    }
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Test: begin");
+    }
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("Test: end");
+    }
+    @Test
+    public void testOddNumbersExterminatorNormalList() {
+        //Given
+                ArrayList<Integer> normalList = new ArrayList<>();
+        normalList.add(1);
+        normalList.add(2);
+        normalList.add(5);
+        normalList.add(4);
+
+        ArrayList<Integer> resultNormalList = new ArrayList<>();
+        resultNormalList.add(2);
+        resultNormalList.add(4);
+        //When
+        ArrayList<Integer> resultList = oddNumbersExterminator.exterminate.(normalList);
+        System.out.println("Test - normal list.");
+        //Then
+        Assert.assertEquals(resultNormalList, resultList);
+    }
+    @Test
+    public void testOddNumbersExterminatorEmptyList(){
+        //Given
+        ArrayList<Integer>emptyList = new ArrayList<>();
+        //When
+        ArrayList<Integer>resultEmptyList = oddNumbersExterminator.exterminate.(emptyList);
+        System.out.println("Test - empty list");
+        //Then
+        Assert.assertEquals(emptyList, resultEmptyList);
+
+    }
 }
