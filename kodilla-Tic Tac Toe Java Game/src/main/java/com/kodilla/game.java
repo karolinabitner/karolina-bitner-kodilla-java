0664c48370640be8fb0949aea10244056bd11d3a
Karolina Bitner <karolabitner@gmail.com>
2020-06-16 18:39:09 +0200

    plansza w kółko i krzyżyk

---
+++ kodilla-Tic Tac Toe Java Game/src/main/java/com/kodilla/game.java
@@ -0,0 +1,33 @@
+package com.kodilla;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+
+public class game extends Application {
+    private Image imageback = new Image("file:resources/plansza.png");
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background background = new Background(backgroundImage);
+
+        GridPane grid = new GridPane();
+        grid.setBackground(background);
+
+        Scene scene = new Scene(grid, 720, 714, Color.BLACK);
+
+        primaryStage.setTitle("Tic Tac Toe");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
